c0ee5e0588cf19490d318ff65c16854972f560af
Jonas Rindlisbacher <joriba@gmx.ch>
2023-02-02 22:33:46 +0100

    implement search()

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,57 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    println!("Searching for \"{}\"", config.query);
-    println!("In file {}", config.file_path);
-
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-        
-    println!("With text:\n{}", contents);
+
     Ok(())
 }
 
-pub struct Config <'a>{
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub struct Config<'a> {
     pub query: &'a str,
     pub file_path: &'a str,
 }
 
-impl <'a> Config<'a> {
+impl<'a> Config<'a> {
     pub fn build(args: &[String]) -> Result<Config, String> {
+        // want total of 3 args: binary location (always included), search string, input file
         if args.len() != 3 {
-            return Err(format!("Wrong number of arguments! 2 required, but {} {} found", args.len() - 1,
-                if args.len() == 2 {"was"} else {"were"}));
+            return Err(format!(
+                "Wrong number of arguments! 2 required, but {} {} found",
+                args.len() - 1,
+                if args.len() == 2 { "was" } else { "were" }
+            ));
         }
         let query = &args[1];
         let file_path = &args[2];
         Ok(Config { query, file_path })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
